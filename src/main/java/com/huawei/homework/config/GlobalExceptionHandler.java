300d7ebfada8a95c060c94f7b33258bdfc7a5ce6
Hogan <hsbzzhz@126.com>
2022-10-01 10:58:08 +0800

    git rm index

--- src/main/java/com/huawei/homework/config/GlobalExceptionHandler.java
+++ src/main/java/com/huawei/homework/config/GlobalExceptionHandler.java
@@ -1,7 +1,8 @@
 package com.huawei.homework.config;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.validation.BindException;
-import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -20,11 +21,12 @@ public class GlobalExceptionHandler {
      * @param e
      * @return
      */
+    private static final Logger LOGGER = LoggerFactory.getLogger(GlobalExceptionHandler.class);
     @ExceptionHandler(value = BindException.class)
     @ResponseBody
     public CommonResp validExceptionHandler(BindException e) {
         CommonResp commonResp = new CommonResp();
-//        LOG.warn("参数校验失败：{}", e.getBindingResult().getAllErrors().get(0).getDefaultMessage());
+        LOGGER.warn("参数校验失败：{}", e.getBindingResult().getAllErrors().get(0).getDefaultMessage());
         commonResp.setSuccess(false);
         commonResp.setMessage(e.getBindingResult().getAllErrors().get(0).getDefaultMessage());
         return commonResp;
