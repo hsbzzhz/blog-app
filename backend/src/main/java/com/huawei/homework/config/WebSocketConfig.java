81cd00c897c46ada3eff838d70329d2a78a1bdc1
Hogan <hsbzzhz@126.com>
2024-09-15 18:31:24 +0800

    update gitignore

--- backend/src/main/java/com/huawei/homework/config/WebSocketConfig.java
+++ backend/src/main/java/com/huawei/homework/config/WebSocketConfig.java
@@ -2,7 +2,6 @@ package com.huawei.homework.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.socket.server.standard.ServerEndpointExporter;
 
 /**
  * 开启WebSocket
@@ -10,8 +9,4 @@ import org.springframework.web.socket.server.standard.ServerEndpointExporter;
 @Configuration
 public class WebSocketConfig {
 
-    @Bean
-    public ServerEndpointExporter serverEndPointExporter() {
-        return new ServerEndpointExporter();
-    }
 }
