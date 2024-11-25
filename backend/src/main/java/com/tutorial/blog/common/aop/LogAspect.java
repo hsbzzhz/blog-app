a1d1b809753db69b3d646ed3db6de93ef4d0fa06
Hogan <hsbzzhz@126.com>
2024-11-25 23:14:29 +0800

    feat(日志):AOP记录日志

---
+++ backend/src/main/java/com/tutorial/blog/common/aop/LogAspect.java
@@ -0,0 +1,74 @@
+package com.tutorial.blog.common.aop;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.tutorial.blog.utils.HttpContextUtils;
+import com.tutorial.blog.utils.IpUtils;
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+import org.aspectj.lang.reflect.MethodSignature;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.http.HttpServletRequest;
+import java.lang.reflect.Method;
+
+/**
+ * 日志切面
+ *
+ */
+@Aspect
+@Component
+@Slf4j
+public class LogAspect {
+    @Pointcut("@annotation(com.tutorial.blog.common.aop.LogAnnotation)")
+    public void logPointCut() {
+    }
+
+    @Around("logPointCut()")
+    public Object around(ProceedingJoinPoint point) throws Throwable {
+        long beginTime = System.currentTimeMillis();
+        //执行方法
+        Object result = point.proceed();
+        //执行时长(毫秒)
+        long time = System.currentTimeMillis() - beginTime;
+        //保存日志
+        recordLog(point, time);
+        return result;
+    }
+
+    private void recordLog(ProceedingJoinPoint joinPoint, long time) {
+        MethodSignature signature = (MethodSignature) joinPoint.getSignature();
+        Method method = signature.getMethod();
+        LogAnnotation logAnnotation = method.getAnnotation(LogAnnotation.class);
+        log.info("=====================log start================================");
+        log.info("module:{}",logAnnotation.module());
+        log.info("operation:{}",logAnnotation.operation());
+
+        //请求的方法名
+        String className = joinPoint.getTarget().getClass().getName();
+        String methodName = signature.getName();
+        log.info("request method:{}",className + "." + methodName + "()");
+
+        try {
+            //请求的参数
+            Object[] args = joinPoint.getArgs();
+            ObjectMapper objectMapper = new ObjectMapper();
+
+            String params = objectMapper.writeValueAsString(args[0]);
+            log.info("params:{}",params);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
+
+        //获取request 设置IP地址
+        HttpServletRequest request = HttpContextUtils.getHttpServletRequest();
+        log.info("ip:{}", IpUtils.getIpAddr(request));
+
+
+        log.info("excute time : {} ms",time);
+        log.info("=====================log end================================");
+    }
+}
